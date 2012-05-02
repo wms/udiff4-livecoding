403b209edd4253b80798f82d9c37bde138a7d0a9
warrenseymour <wms@fountainhead-solutions.net>
2012-05-02 18:20:17 +0100

    Add Lithium submodule and load in config

--- config.php
+++ config.php
@@ -27,4 +27,34 @@ define('DB_USERNAME', 'udiff4-livecodin');
 define('DB_PASSWORD', 'UPxKNQmdGGh4ntqs');
 define('DB_DATABASE', 'udiff4-livecodin');
 define('DB_PREFIX', '');
+
+// Path to Lithium library
+define('LITHIUM_LIBRARY_PATH', dirname(__DIR__) .  '/libraries/lithium/');
+define('LITHIUM_APP_PATH', dirname(__DIR__));
+
+// Load Lithium library and application
+if(!include LITHIUM_LIBRARY_PATH . 'core/Libraries.php') {
+    trigger_error('Could not load Lithium', E_USER_ERROR);
+}
+
+use lithium\core\Libraries;
+
+// Lithium core libraries
+Libraries::add('lithium');
+
+// Application Lithium code
+Libraries::add('udiff4-livecoding', array(
+    'default' => true
+));
+
+use lithium\data\Connections;
+
+Connections::add('default', [
+    'type' => 'database',
+    'adapter' => 'MySql',
+    'host' => DB_HOSTNAME,
+    'database' => DB_DATABASE,
+    'login' => DB_USERNAME,
+    'password' => DB_PASSWORD
+]);
 ?>
