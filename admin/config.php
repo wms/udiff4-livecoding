2a246d5c04fde97e8392f795a0aa72c96de62904
Warren Seymour <wms@fountainhead-solutions.net>
2012-05-02 15:52:18 +0100

    Initial commit of OpenShite

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/udiff4-livecoding/admin/');
+define('HTTP_CATALOG', 'http://localhost/udiff4-livecoding/');
+define('HTTP_IMAGE', 'http://localhost/udiff4-livecoding/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/udiff4-livecoding/admin/');
+define('HTTPS_IMAGE', 'http://localhost/udiff4-livecoding/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/udiff4-livecoding/admin/');
+define('DIR_SYSTEM', '/var/www/udiff4-livecoding/system/');
+define('DIR_DATABASE', '/var/www/udiff4-livecoding/system/database/');
+define('DIR_LANGUAGE', '/var/www/udiff4-livecoding/admin/language/');
+define('DIR_TEMPLATE', '/var/www/udiff4-livecoding/admin/view/template/');
+define('DIR_CONFIG', '/var/www/udiff4-livecoding/system/config/');
+define('DIR_IMAGE', '/var/www/udiff4-livecoding/image/');
+define('DIR_CACHE', '/var/www/udiff4-livecoding/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/udiff4-livecoding/download/');
+define('DIR_LOGS', '/var/www/udiff4-livecoding/system/logs/');
+define('DIR_CATALOG', '/var/www/udiff4-livecoding/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'udiff4-livecodin');
+define('DB_PASSWORD', 'UPxKNQmdGGh4ntqs');
+define('DB_DATABASE', 'udiff4-livecodin');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/udiff4-livecoding/');
+define('HTTP_IMAGE', 'http://localhost/udiff4-livecoding/image/');
+define('HTTP_ADMIN', 'http://localhost/udiff4-livecoding/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/udiff4-livecoding/');
+define('HTTPS_IMAGE', 'http://localhost/udiff4-livecoding/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/udiff4-livecoding/catalog/');
+define('DIR_SYSTEM', '/var/www/udiff4-livecoding/system/');
+define('DIR_DATABASE', '/var/www/udiff4-livecoding/system/database/');
+define('DIR_LANGUAGE', '/var/www/udiff4-livecoding/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/udiff4-livecoding/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/udiff4-livecoding/system/config/');
+define('DIR_IMAGE', '/var/www/udiff4-livecoding/image/');
+define('DIR_CACHE', '/var/www/udiff4-livecoding/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/udiff4-livecoding/download/');
+define('DIR_LOGS', '/var/www/udiff4-livecoding/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'udiff4-livecodin');
+define('DB_PASSWORD', 'UPxKNQmdGGh4ntqs');
+define('DB_DATABASE', 'udiff4-livecodin');
+define('DB_PREFIX', '');
+?>
